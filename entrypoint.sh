e860b741c0dcf9fb6a0d71811b0b886445df8eb4
Zunaid Amin Enan <zunaid321@gmail.com>
2021-07-29 07:03:18 +0600

    fixed project name

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn django_project.wsgi:application --bind 0.0.0.0:8000
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000
 
