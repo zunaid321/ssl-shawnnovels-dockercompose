08b88dec44fa86e401928d4c24b2b24f5428926e
Zunaid Amin Enan <zunaid321@gmail.com>
2021-07-30 01:36:47 +0600

    changed database

--- django_project/backend/settings.py
+++ django_project/backend/settings.py
@@ -90,17 +90,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'postgres',
-       'USER': 'admin',
-       'PASSWORD': 'Shawndb2021!',
-       'HOST': 'hdm.tnz.mybluehost.me',
-       'PORT': '5432',
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'postgres',
+#        'USER': 'admin',
+#        'PASSWORD': 'Shawndb2021!',
+#        'HOST': 'hdm.tnz.mybluehost.me',
+#        'PORT': '5432',
+#    }
+# }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
