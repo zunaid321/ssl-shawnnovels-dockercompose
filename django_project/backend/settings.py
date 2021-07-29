68d472d13e11ccbcb8a6b7070b03c10928e6266e
Zunaid Amin Enan <zunaid321@gmail.com>
2021-07-29 18:15:34 +0600

    Connected with Postgresql

--- django_project/backend/settings.py
+++ django_project/backend/settings.py
@@ -84,10 +84,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'postgres',
+       'USER': 'admin',
+       'PASSWORD': 'Shawndb2021!',
+       'HOST': 'hdm.tnz.mybluehost.me',
+       'PORT': '5432',
+   }
 }
 
 
