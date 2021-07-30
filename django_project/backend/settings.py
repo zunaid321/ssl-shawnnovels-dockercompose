c48179cc7bdc2cca5c14004fd096073acf9784eb
Zunaid Amin Enan <zunaid321@gmail.com>
2021-07-30 15:25:45 +0600

    Update settings.py

--- django_project/backend/settings.py
+++ django_project/backend/settings.py
@@ -161,5 +161,7 @@ CORS_ALLOWED_ORIGINS = [
 STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = '/vol/web/media'
 STATIC_ROOT = '/vol/web/static'
