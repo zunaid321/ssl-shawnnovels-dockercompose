5237c25b0be12c63b515e574a366acedd2b0f2d0
ashiqurrah7 <ashiqur.rahm@gmail.com>
2021-07-31 22:16:37 +0600

    lawyer migration added

--- django_project/backend/settings.py
+++ django_project/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'c_r-e8v1divj8y+hu@-w=n#$xj#ciuejybd3_(k2h789(mcv8$'
+# SECRET_KEY = 'c_r-e8v1divj8y+hu@-w=n#$xj#ciuejybd3_(k2h789(mcv8$'
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
