748426325c1738b3b553a83f40e4c499fc95ce84
Ashiqur Rahman <ashiqur.rahm@gmail.com>
2021-07-31 22:27:36 +0600

    Update models.py

--- django_project/base/models.py
+++ django_project/base/models.py
@@ -67,7 +67,7 @@ class Lawyer(models.Model):
         ('Associates', 'Associates'),
         ('Councel', 'Councel'),
     ]
-#     type = models.CharField(max_length=11, choices=LAWYER_CHOICES, default= 'Councel')
+    type = models.CharField(max_length=11, choices=LAWYER_CHOICES, default= 'Councel')
     name = models.CharField(max_length=100, default='')
     picture = models.ImageField(upload_to=upload_lawyer_image, null =True)
 #     credentials = ArrayField(ArrayField(models.CharField(max_length=150, blank=True)), size= 1)
