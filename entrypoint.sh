fe7495a35e3b379529462fde526b005cd71a7dfb
Zunaid Amin Enan <zunaid321@gmail.com>
2021-08-10 03:44:51 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,5 @@ python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn backend.wsgi:application --bind 0.0.0.0:8000
+gunicorn --worker-class = gevent --worker-connections = 1000 --workers = 3 backend.wsgi:application --bind 0.0.0.0:8000
 
